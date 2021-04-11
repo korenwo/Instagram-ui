af131368b0220a57f9280be2f89b0509b2c83f41
koren wohl <korenwohl@gmail.com>
2021-04-11 13:54:22 +0300

    changedesgin

--- src/common/PostLike/PostLike.js
+++ src/common/PostLike/PostLike.js
@@ -1,19 +1,13 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState} from 'react';
 import './PostLike.scss';
 import { PostService } from '../../services/post.service';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { UserContext } from '../../user-context';
 
 function PostLike ({post}) {
  
     const [likes, setLikes] = useState (post?.likes?.length || 0 );
     const [liked, setLiked] = useState(false);
-    const { user } = useContext(UserContext);
-
-    useEffect(()=> {
-        setLiked(post.likes.includes(user._id));
-    });
 
     async function addLike () {
         const res = await PostService.like(post._id);
@@ -23,7 +17,7 @@ function PostLike ({post}) {
 
     return (
         <div>
-            <a onClick={addLike}><FontAwesomeIcon icon={ faHeart } color={liked ? 'red' : 'white'} /></a> 
+            <div onClick={addLike}><FontAwesomeIcon icon={ faHeart } color={liked ? 'red' : 'white'} /></div> 
             <div className="likes">Likes {likes}</div>
         </div>
     );
