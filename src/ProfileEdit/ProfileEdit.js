9b6f9e6f119bfbcd4dfbed4f2e6ff682fb484323
koren wohl <korenwohl@gmail.com>
2021-04-13 01:31:20 +0300

    changes

--- src/ProfileEdit/ProfileEdit.js
+++ src/ProfileEdit/ProfileEdit.js
@@ -46,33 +46,6 @@ return (
                             <ErrorMessage name="password" component="div" className="error" />
                         </div>
 
-                        <div className="form-group mb-3">
-                            <label htmlFor="web">Website</label>
-                            <Field type="web" className="form-control" id="web" name="web" />
-                        </div>
-
-                        <div className="form-group mb-3">
-                            <label htmlFor="bio">Bio</label>
-                            <Field type="bio" className="form-control" id="bio" name="bio" />
-                        </div>
-
-                        <div className="form-group mb-3">
-                            <label htmlFor="mail">Email</label>
-                            <Field type="mail" className="form-control" id="mail" name="mail" />
-                            <ErrorMessage name="mail" component="div" className="error" />
-                        </div>
-
-                        <div className="form-group mb-3">
-                            <label htmlFor="phone">Phone Number</label>
-                            <Field type="phone" className="form-control" id="phone" name="phone" />
-                            <ErrorMessage name="phone" component="div" className="error" />
-                        </div>
-
-                        <div className="form-group mb-3">
-                            <label htmlFor="gen">Gender</label>
-                            <Field type="gen" className="form-control" id="gen" name="gen" />
-                        </div>    
-
                         <button type="submit" className="submitBtn" disabled={isSubmitting}>{isSubmitting ? 'Saving...' : 'submit' }</button>
                     </Form>
                 )}
